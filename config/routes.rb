e8737ba1568ee19cff53a344022f34be9747f974
Luca G. Soave <luca.soave@gmail.com>
2009-01-18 12:00:21 -0700

    minor fixing into HTTPS routings

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ActionController::Routing::Routes.draw do |map|
 
   #map.resources :reports, :collection => {:population => :get}
 
-  map.resources :lifestream, :controller => "home", :path_prefix => '/:account', :requirements => { :protocol => "http" }
+  map.resources :lifestream, :controller => "home", :path_prefix => '/:account'#, :requirements => { :protocol => "https" }
   map.resources :feed_urls, :path_prefix => '/:account', :requirements => { :protocol => "http" }
   map.connect '/search', :controller => 'home', :action => 'search', :requirements => { :protocol => "http" }
 
@@ -14,26 +14,26 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resource :account, :requirements => { :protocol => "https" },
     :member => { :activate => :get, :password => :get, :change_password => :put } do |account|
-    account.resources :personas do |personas|
+    account.resources :personas, :requirements => { :protocol => "https" } do |personas|
       personas.resources :properties
     end
-    account.resources :sites do |sites|
+    account.resources :sites, :requirements => { :protocol => "https" } do |sites|
       sites.resources :release_policies
     end
-    account.resource :yubikey_association
+    account.resource :yubikey_association, :requirements => { :protocol => "https" }
   end
   
   map.resource :session, :requirements => { :protocol => "https" }
   map.resource :password, :requirements => { :protocol => "http" }
   
   map.with_options :controller => 'passwords' do |pwd|
-    pwd.forgot_password 'forgot_password', :action => 'new'
-    pwd.reset_password  'reset_password/:id', :action => 'edit'
+    pwd.forgot_password 'forgot_password', :action => 'new', :protocol => "https"
+    pwd.reset_password  'reset_password/:id', :action => 'edit', :protocol => "https"
   end
 
   map.with_options :controller => 'sessions' do |sessions|
     sessions.login 'login', :action => 'new', :protocol => 'https'
-    sessions.logout 'logout', :action => 'destroy', :protocol => 'https'
+    sessions.logout 'logout', :action => 'destroy', :protocol => 'http'
   end
 
   #map.with_options :controller => 'sessions' do |sessions|
@@ -42,14 +42,14 @@ ActionController::Routing::Routes.draw do |map|
   #end
 
   map.with_options :controller => 'server' do |server|
-    server.formatted_server 'server.:format', :action => 'index'
-    server.server 'server', :action => 'index'
-    server.decide 'server/decide', :action => 'decide'
-    server.proceed 'server/proceed', :action => 'proceed'
-    server.complete 'server/complete', :action => 'complete'
-    server.cancel 'server/cancel', :action => 'cancel'
-    server.formatted_seatbelt_config 'server/seatbelt/config.:format', :action => 'seatbelt_config'
-    server.formatted_seatbelt_state 'server/seatbelt/state.:format', :action => 'seatbelt_login_state'
+    server.formatted_server 'server.:format', :action => 'index', :protocol => "http"
+    server.server 'server', :action => 'index', :protocol => "http"
+    server.decide 'server/decide', :action => 'decide', :protocol => "http"
+    server.proceed 'server/proceed', :action => 'proceed', :protocol => "http"
+    server.complete 'server/complete', :action => 'complete', :protocol => "http"
+    server.cancel 'server/cancel', :action => 'cancel', :protocol => "http"
+    server.formatted_seatbelt_config 'server/seatbelt/config.:format', :action => 'seatbelt_config', :protocol => "http" 
+    server.formatted_seatbelt_state 'server/seatbelt/state.:format', :action => 'seatbelt_login_state', :protocol => "http"
   end
 
   map.with_options :controller => 'consumer' do |consumer|
