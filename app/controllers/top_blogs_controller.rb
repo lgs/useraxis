3f6fce7c88599e653189cb0727d14152428cc0aa
Luca G. Soave <luca.soave@gmail.com>
2009-03-24 13:07:37 -0600

    Quick & Dirty - Delicious TAG Lookup - implementation

--- app/controllers/top_blogs_controller.rb
+++ app/controllers/top_blogs_controller.rb
@@ -1,34 +1,31 @@
 class TopBlogsController < ApplicationController
-  logger.debug "CONTROLLER : TopBlogsController "
+logger.debug "CONTROLLER : TopBlogsController "
 
   DELICIOUS_URL_ENGINE = "http://delicious.com/url"
 
   # GET /top_blogs
   def index(page = params[:url_lookup])
+    logger.debug "TopBlogsController#index - params[:url_lookup] : #{params[:url_lookup]}"
     logger.debug "TopBlogsController#index - page : #{page}"
-
-    @extractor = Scrubyt::Extractor.new do
+    return if page == nil
+      @extractor = Scrubyt::Extractor.new do
           fetch DELICIOUS_URL_ENGINE
           fill_textfield "url", page 
           submit
           # "//html/body[@id='view']/div[@id='doc3']/div[@id='bd']/div[@id='sidebar']/div[@id='top-tags']/ul/li/a[@class='showTag']/span[@class='m']/"
           tag "//li/a[@class='showTag']/span[@class='m']/"
-    end
-    # .first(10) extracts 5 key-values couple 
-    # Max 5 tags, cause delicious TagLookup limits the query construction to 5 tags
+      end
 
-    #@list = TopBlog.list(@extractor.results.first(10).inspect)
-    @tmp = TopBlog.list(@extractor.results.first(10))
-    logger.debug "TopBlogsController#index - @top_blogs : #{@tmp}"
+      # .first(10) extracts 5 key-values couple 
+      # Max 5 tags, cause delicious TagLookup limits the query construction to 5 tags
 
-
-
-
-    #@top_blogs = TopBlog.find(params[:url_lookup])
-    #@top_blogs = DeliciousUrlLookup.find(params[:url_lookup])
-    #logger.debug "TopBlogsController#show - params[:url_lookup] : #{params[:url_lookup]}"
-    #logger.debug "TopBlogsController#show - @top_blogs = TopBlog.find(params[:url_lookup]) : #{@top_blogs.attributes.inspect}"
-    #logger.debug "TopBlogsController#show - params[:url_lookup] : #{@top_blogs.attributes.inspect}"
+      logger.debug "TopBlogsController#index - @extractor : #{@extractor.results.first(10)}"
+      tmp = TopBlog.list(@extractor.results.first(10))
+      logger.debug "TopBlogsController#index - tmp.class : #{tmp.class}"
+      logger.debug "TopBlogsController#index - tmp : #{tmp}"
+      @list = tmp.split("\t")
+      logger.debug "TopBlogsController#index - @list.class : #{@list.class}"
+      logger.debug "TopBlogsController#index - end - @list : #{@list}"
   end
 
   def show
--- app/models/top_blog.rb
+++ app/models/top_blog.rb
@@ -11,48 +11,29 @@ class TopBlog #< Scrubyt::Extractor
 
   def self.list(addtag)
     self.tags(addtag)
-    #RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list(addtag) == addtag : #{addtag}"
     doc = Hpricot(open("http://delicious.com/tag/#{@tags}"))
-      (doc/"//ul[@id='bookmarklist']/li/div/div/h4/a").each do |href|
-         @list = puts href.attributes['href']
-         #puts href.inner_html
-         RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list - inner doc/ loop : #{@list}"
-       end
-    RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list(addtag) == @list : #{@list}"
+    RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list  - Hpricot - @tags : #{@tags}"
+    @tmp = "\t"
+    RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list - @tmp = \"\" : #{@tmp.class}"
+    (doc/"//ul[@id='bookmarklist']/li/div/div/h4/a").each do |href|
+       @tmp << href.attributes['href'] + "\t"
+    end
+    RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list - end - @tmp << href.attributes['href'] + \"\t\" : #{@tmp.class}"
+    RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list - doc/ loop ended - @tmp : #{@tmp}"
+    #@output = @tmp.split("\t")
+    #RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list - end - @output.class : #{@output.class}"
+    #RAILS_DEFAULT_LOGGER.debug "MODEL : TopBlog.list - end - @output = @tmp.split(\"\t\") : #{@output}"
+    #@output
+    @tmp
   end
 
   protected
   def self.tags(addtag)
-    RAILS_DEFAULT_LOGGER.debug "MODEL protected : TopBlog.tags(addtag) == addtag : #{addtag}"
+    RAILS_DEFAULT_LOGGER.debug "MODEL protected : TopBlog - self.tags(addtag) == addtag : #{addtag}"
     @tags = "#{addtag[0][:tag]}+#{addtag[2][:tag]}+#{addtag[4][:tag]}+#{addtag[6][:tag]}+#{addtag[8][:tag]}"
-    RAILS_DEFAULT_LOGGER.debug "MODEL protected : TopBlog - @tags : #{@tags}"
+    RAILS_DEFAULT_LOGGER.debug "MODEL protected : TopBlog - self.tags - @tags : #{@tags}"
   end
 end
-
-########################################################################################################################
-  ### Actually extracted and swapped on the controller ( Yes, bad thing ), just to work
-#  def initialize(page = params[:url_lookup])
-#    RAILS_DEFAULT_LOGGER.debug "TopBlog.page : #{page}"
-#    @extractor = Scrubyt::Extractor.new do
-#          fetch DELICIOUS_URL_ENGINE
-#          fill_textfield "url", page
-#          submit
-#          # "//html/body[@id='view']/div[@id='doc3']/div[@id='bd']/div[@id='sidebar']/div[@id='top-tags']/ul/li/a[@class='showTag']/span[@class='m']/"
-#          tag "//li/a[@class='showTag']/span[@class='m']/"
-#    end
-#  end
-
-  #def self.url_lookup(page)
-  #RAILS_DEFAULT_LOGGER.debug "TopBlog#url_lookup - page : #{page}"
-
-    #@extractor = superclass.new do #Scrubyt::Extractor.new do
-    #      fetch DELICIOUS_URL_ENGINE
-    #      fill_textfield "url", page
-    #      submit
-    #      # "//html/body[@id='view']/div[@id='doc3']/div[@id='bd']/div[@id='sidebar']/div[@id='top-tags']/ul/li/a[@class='showTag']/span[@class='m']/"
-    #      tag "//li/a[@class='showTag']/span[@class='m']/"
-    #end
-  #end
 
 # Ex controller#index
 
@@ -66,4 +47,3 @@ end
 #          # "//html/body[@id='view']/div[@id='doc3']/div[@id='bd']/div[@id='sidebar']/div[@id='top-tags']/ul/li/a[@class='showTag']/span[@class='m']/"
 #          tag "//li/a[@class='showTag']/span[@class='m']/"
 #    end
-#
